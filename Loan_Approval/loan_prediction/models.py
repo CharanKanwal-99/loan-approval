d94de1ad94218860e6aca3f904e863f50aa51b5c
CharanKanwal-99 <111955279+CharanKanwal-99@users.noreply.github.com>
2023-08-05 23:53:34 +0530

    Update models.py

--- Loan_Approval/loan_prediction/models.py
+++ Loan_Approval/loan_prediction/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class MyModel(models.Model):
+    Gender = models.CharField()
+    Married = models.CharField()
+    Dependents = models.IntegerField()
+    Education = models.CharField()
+    Self_Employed = models.CharField()
+    Applicant_Income = models.IntegerField()
+    Coapplicant_Income = models.IntegerField()
+    Loan_Amount = models.IntegerField()
+    Loan_Amount_Term = models.IntegerField()
+    Credit_History = models.CharField()
+    Property_Area = models.CharField()
